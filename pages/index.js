a0d88e3ad1e8cc521cd31240cbfa1b96ffccb39f
James Whyte <jswhyte@msn.com>
2022-02-15 20:20:01 +0000

    Basic fetch API call.

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,30 @@ import { useState } from 'react'
 export default function Home() {
 
   const [inputValue, setInputValue] = useState('')
-  const enrichData = async () => {}
+  const [enrichedData, setEnrichedData] = useState([])
+
+  console.log('\n\n\n\n\ enrichData', enrichedData)
+
+  const enrichData = async () => {
+    const response = await fetch('https://app.herondata.io/api/merchants/extract', {
+      method: 'POST',
+      body: JSON.stringify({ description: inputValue }),
+      headers: { 
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonObject) => {
+        console.log(jsonObject)
+        enrichedData.push(jsonObject)
+        console.log('\n\n\n\n\ enrichData', enrichedData)
+      })
+      .catch((error) => {
+        document.write(error);
+      });
+  }
 
   return (
     <div className="container">
@@ -20,11 +43,15 @@ export default function Home() {
           <div className="enrich-demo__intro">
             <h1>Try us</h1>
             <p>This is a free demo of our merchant enrichment product used to create a beautiful transaction feed. Enter a bank transaction or try our examples.</p>
+            <p>Example: SALESFORCE CRM - SIN HTTPSNINJAFOR TN 08/06</p>
           </div>
-          <form className="enrich-demo__input">
+          <div className="enrich-demo__input">
             <input type='text' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
             <button onClick={enrichData}>Enrich</button>
-          </form>
+          </div>
+          <div className="enrich-demo__results">
+            Results go here.
+          </div>
         </div>
       </main>
     </div>
