ec606d04b47174dac5e4fe2621ee8171feb77d9f
James Whyte <jswhyte@msn.com>
2022-02-15 19:56:35 +0000

    Layout + basic styling.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eb43d1d9e105dc59"; }
+/******/ 		__webpack_require__.h = function() { return "c0ed192d6abd71f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
